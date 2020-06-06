9620b63338fe4b1f11224b70407bee011cca9e86
sepenadiaz <se.penadiaz@gmail.com>
2020-06-06 12:50:23 -0300

    Primer commit Seba

--- TicketDealer/src/main/java/Init/Main.java
+++ TicketDealer/src/main/java/Init/Main.java
@@ -14,6 +14,7 @@ public class Main {
     
     public static void main(String args[]) {
         System.out.println("Probando");
+        System.out.println("Commit Seba");
         Iniciar();
     }
     public static void Iniciar(){
