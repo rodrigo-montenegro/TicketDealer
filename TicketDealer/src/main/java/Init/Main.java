75e8fa7fb8d762d02044e05eb2d62dbc010df825
santiagoiannelli <65088707+santiagoiannelli@users.noreply.github.com>
2020-06-06 18:39:24 -0300

    santi
     agregue el commit santi

--- TicketDealer/src/main/java/Init/Main.java
+++ TicketDealer/src/main/java/Init/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Probando");
         System.out.println("Commit Seba");
+        System.out.println("Commit Santi");
         Iniciar();
     }
     public static void Iniciar(){
