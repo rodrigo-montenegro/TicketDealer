4302d9afc34eac82bad17f88872002004c39118e
sepenadiaz <62730027+sepenadiaz@users.noreply.github.com>
2020-06-24 20:28:10 -0300

    Agregado constructor y 3 metodos

--- TicketDealer/src/main/java/Controlador/ControllerAdmin.java
+++ TicketDealer/src/main/java/Controlador/ControllerAdmin.java
@@ -9,6 +9,26 @@ package Controlador;
  *
  * @author Esteban
  */
-public class ControllerAdmin {
-    
-}
+public class ControllerAdmin implements ControllerInterface {
+  Cargador model;
+  Home view;
+
+  public ControllerAdmin(Cargador model, Home view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  @Override
+  public void iniciar() { // OK!
+  }
+
+  @Override
+  public void cambiarALogin(Home v) {// OK!
+    v.setVisible(false);
+    Login l = new Login(this);
+    l.setVisible(true);
+  }
+
+  @Override
+	public void cambiarAHome2(Home v) {	//OK!
+	}
