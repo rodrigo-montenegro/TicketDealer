089ea44a1c81d1649f801cae181969b8dbb0fdcd
santiagoiannelli <65088707+santiagoiannelli@users.noreply.github.com>
2020-06-25 01:02:58 -0300

    cambio HomeAdmin

--- TicketDealer/src/main/java/Vistas/HomeAdmin.java
+++ TicketDealer/src/main/java/Vistas/HomeAdmin.java
@@ -4,20 +4,24 @@
  * and open the template in the editor.
  */
 package Vistas;
-
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import main.java.controlador.*;
+import main.java.controlador.ControllerInterface;
 /**
  *
  * @author santi
  */
 public class HomeAdmin extends javax.swing.JFrame {
-
-    /**
-     * Creates new form HomeAdmin1
-     */
-    public HomeAdmin() {
+ControllerInterface controller;
+	
+    public HomeAdmin(ControllerInterface controller) {
+    	this.controller=controller;
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -123,59 +127,23 @@ public class HomeAdmin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-          Login login = new Login();
-        login.setVisible(true);
-        this.setVisible(false);
+        controller.cambiarALogin(this);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-         cargaStock stocks= new cargaStock();
-        stocks.setVisible(true);
-        this.setVisible(false);
+      controller.cambiarACargaStock(this);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        ConsultaStock stocks= new ConsultaStock();
-        stocks.setVisible(true);
-        this.setVisible(false);
+        controller.cambiarAConsultaStock(this);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomeAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new HomeAdmin().setVisible(true);
-            }
-        });
-    }
+   
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
